5d26baf9717a2feb77cd357e90ba2cf013e130a3
khyathi18 <ksri0307@gmail.com>
2022-05-25 13:47:06 +0530

    firstappcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ParaComponent } from './para/para.component';
+import { BtnComponent } from './btn/btn.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParaComponent,
+    BtnComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
